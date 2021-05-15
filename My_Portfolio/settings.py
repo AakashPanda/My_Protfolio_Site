8cd44f923245ca8501b7a9f1457618b57ce4ab8c
Aakash panda <aakashpanda81@gmail.com>
2021-05-16 00:58:43 +0530

    Added Requirements.txt

--- My_Portfolio/settings.py
+++ My_Portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'My_Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Portfolio_Database',
+        'USER': 'postgres',
+        'PASSWORD': 'aakash2000',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
