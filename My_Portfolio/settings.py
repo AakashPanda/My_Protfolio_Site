413e7e5bb0fc9f90fc310820c7b136684f00b450
Aakash panda <aakashpanda81@gmail.com>
2021-05-16 01:40:27 +0530

    Changes

--- My_Portfolio/settings.py
+++ My_Portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'aakashpanda232@gmail.com'
 EMAIL_HOST_PASSWORD = 'aakash2000'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
